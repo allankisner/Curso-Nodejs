b882afc7d0626fe511ab3f82c2526c09a9587a42
Allan Kisner <kisner.dev@gmail.com>
2022-09-30 17:00:35 -0300

    conclusao_sequelize

--- 9_Sequelize/index.js
+++ 9_Sequelize/index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 
 const app = express()
-
 const conn = require('./db/conn')
 
+const Address = require('./models/Address')
 const User = require('./models/User')
 
 app.engine('handlebars', exphbs.engine())
@@ -21,10 +21,10 @@ app.use(express.json())
 app.use(express.static('public'))
 
 
-app.get('/', async(req, res)=>{
-    const users = await User.findAll({raw: true})
+app.get('/', async (req, res) => {
+  const users = await User.findAll({ raw: true })
 
-    res.render('home', { users: users })
+  res.render('home', { users: users })
 })
 
 app.get('/users/create', function (req, res) {
@@ -45,10 +45,107 @@ app.post('/users/create', function (req, res) {
   res.redirect('/')
 })
 
+app.get('/users/:id', async (req, res) => {
+  const id = req.params.id
+
+  const user = await User.findOne({ raw: true, where: { id: id } })
+
+  res.render('userview', { user })
+})
+
+app.post('/users/delete/:id', async (req, res) => {
+  id = req.params.id
+
+  await User.destroy({ where: { id: id } })
+
+  res.redirect('/')
+
+})
+
+app.get('/users/edit/:id', async (req, res) => {
+  id = req.params.id
+
+  try {
+
+    const user = await User.findOne({ include: Address, where: { id: id } })
+
+    res.render('useredit', { user: user.get({ plain: true }) })
+  } catch (error) {
+
+    console.log(error)
+
+  }
+})
+
+app.post('/users/update', function (req, res) {
+  const id = req.body.id
+  const name = req.body.name
+  const occupation = req.body.occupation
+  let newsletter = req.body.newsletter
+
+  if (newsletter === 'on') {
+    newsletter = true
+  } else {
+    newsletter = false
+  }
+
+  const userData = {
+    id,
+    name,
+    occupation,
+    newsletter,
+  }
+
+  console.log(req.body)
+  console.log(userData)
+
+  User.update(userData, {
+    where: {
+      id: id,
+    },
+  })
+    .then((user) => {
+      console.log(user)
+      res.redirect('/')
+    })
+    .catch((err) => console.log(err))
+})
+
+app.post('/address/create', async function (req, res) {
+  const UserId = req.body.UserId
+  const street = req.body.street
+  const number = req.body.number
+  let city = req.body.city
+
+  const address = {
+    UserId,
+    street,
+    number,
+    city,
+  }
+
+  await Address.create(address)
+
+  res.redirect(`/users/edit/${UserId}`)
+})
+
+app.post('/address/delete', async (req, res) => {
+  const UserId = req.body.UserId
+  const id = req.body.id;
+
+  await Address.destroy({
+    where: { id: id },
+  })
+
+  res.redirect(`/users/edit/${UserId}`)
+})
+
+
 
 // Criar tabelas e rodar o app
 conn
   .sync()
+  //  .sync({force: true})
   .then(() => {
     app.listen(3000)
   })
